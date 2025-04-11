f3e66e90c1278371a7f47baded3826341c0b01fb
kriskris-27 <krishnakumars0101@gmail.com>
2025-04-11 09:30:00 +0530

    add

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -41,7 +41,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "group relative flex h-[5rem] w-full items-center justify-center bg-white dark:bg-black",
+        "group relative flex h-[3rem] w-full items-center justify-center bg-black dark:bg-black",
         containerClassName,
       )}
       onMouseMove={handleMouseMove}
