53c3a952978859c06910bc3e497f66a8bca160c8
kriskris-27 <krishnakumars0101@gmail.com>
2025-04-13 13:12:47 +0530

    corre ted

--- src/components/ui/hero-highlight.tsx
+++ src/components/ui/hero-highlight.tsx
@@ -130,7 +130,8 @@ export const Highlight = ({
         display: "inline",
       }}
       className={cn(
-        `relative inline-block rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 px-1 pb-1 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block rounded-lg bg-gradient-to-r from-zinc-600 to-zinc-800
+`,
         className,
       )}
     >
--- src/ui-comp/herohigh.tsx
+++ src/ui-comp/herohigh.tsx
@@ -1,28 +1,33 @@
 "use client";
-import { motion } from "motion/react";
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 import { HeroHighlight, Highlight } from "../components/ui/hero-highlight";
 
 export function HeroHighlightDemo() {
+  const { scrollY } = useViewportScroll();
+  // When scrollY goes from 100 to 300, opacity moves from 0 to 1
+  const opacity = useTransform(scrollY, [100, 300], [0, 1]);
+  // When scrollY goes from 100 to 300, shift y from 20px to 0px
+  const y = useTransform(scrollY, [100, 300], [20, 0]);
+
   return (
     <HeroHighlight>
       <motion.h1
+        style={{
+          opacity: opacity,
+          y: y,
+        }}
         initial={{
           opacity: 0,
           y: 20,
         }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
         transition={{
-          duration: 0.5,
+                      duration: 1, // increased duration for a slower effect
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="text-xl md:text-2xl lg:text-2xl font-bold text-neutral-700 dark:text-white max-w-2xl leading-relaxed lg:leading-snug text-center mx-auto"
       >
-        {" "}
         <Highlight className="text-black dark:text-white">
-          scroll scroll scroll
+          Authentic
         </Highlight>
       </motion.h1>
     </HeroHighlight>
